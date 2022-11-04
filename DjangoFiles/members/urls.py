ba6ce67ab2f6c96a69fd7942c312d7df0a08febc
Subhajit009iitr <biswassubhajit009@gmail.com>
2022-11-04 22:11:42 +0530

    corrected

--- DjangoFiles/members/urls.py
+++ DjangoFiles/members/urls.py
@@ -18,28 +18,4 @@ urlpatterns = [
     path('itemAdded/',views.itemAdded,name='itemAdded'),
     path('bidRequest/',views.bidRequest, name='bidRequest'),
     path('bidAccept/',views.bidAccept, name='bidAccept'),   
-=======
-from django.urls import path
-
-from members.views import adminLoggedIn, auctionPortalItems
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('clientLogin/',views.clientLogin, name='clientLogin'),
-    path('clientRegister/',views.clientRegister, name='clientRegister'),
-    path('adminLogin/',views.adminLogin, name='adminLogin'),
-    path('adminRegister/',views.adminRegister, name='adminRegister'),
-    path('adminLoggedIn/',views.adminLoggedIn, name='adminLoggedIn'),
-    path('clientLoggedIn/',views.clientLoggedIn, name='clientLoggedIn'),
-    path('clientRegistered/',views.clientRegistered, name='clientRegistered'),
-    path('adminRegistered/',views.adminRegistered, name='adminRegistered'),
-    path('auctionPortalItems/',views.auctionPortalItems,name='auctionPortalItems'),
-    path('bidUpdate/',views.bidUpdate,name='bidUpdate'),
-    path('itemAdded/',views.itemAdded,name='itemAdded'),
-    path('addItem/',views.addItem,name='addItem'),
-    path('bidRequest/',views.bidRequest, name='bidRequest'),
-    path('bidAccept/',views.bidAccept, name='bidAccept'),   
-    path('bidReject/',views.bidReject, name='bidReject'),
->>>>>>> caa166e59075229df6d6e12bbb55a762303ab609:members/urls.py
-]
+    ]
